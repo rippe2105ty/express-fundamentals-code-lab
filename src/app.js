6e9e6a71b701a70d3dfc70821657dec4622c9a63
rippe2105 <rippe2105@gmail.com>
2023-09-20 11:34:24 -0500

    express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+
+// Ruta raíz que responde con "Hola mundo" en un método GET
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+// Ruta "/mensaje" que responde con el mensaje específico en un método GET
+app.get("/mensaje", (req, res) => {
+  res.send("este es mi servidor usando express");
+});
+
 module.exports = app;
