69df136d1c026ed878ff2b9da67a7a2a6f0a4485
rippe2105 <rippe2105@gmail.com>
2023-10-25 13:12:39 -0500

    cambio

--- src/app.js
+++ src/app.js
@@ -1,22 +1,81 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
+app.use(express.json()); // Para procesar JSON en las solicitudes
 
-const users = [
-  { email: "admin@example.com", name: "admin", rol: "admin" },
-  { email: "user@example.com", name: "user", rol: "user" },
-];
+// Resto de tu configuración de Express...
 
-app.use(express.json());
+// Ruta para autenticación
+app.post('/auth', (req, res) => {
+  const { email } = req.body;
 
-app.get("/", function (req, res) {
-  res.send("Bienvenido a la api de ADA Cars");
+  // Verifica si el email está registrado en el arreglo de usuarios
+  const users = [/* Tu arreglo de usuarios */];
+  const user = users.find((u) => u.email === email);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid user name or password' });
+  }
+
+  // Si el usuario existe, firma un token JWT
+  const token = jwt.sign({ email: user.email, rol: user.rol, name: user.name }, process.env.SECRET_KEY, {
+    algorithm: 'HS256',
+  });
+
+  // Envía el token como respuesta
+  res.json({ token });
 });
 
+// Función de middleware para validar tokens JWT
+function JWTValidation(req, res, next) {
+  const token = req.headers.authorization;
 
-//Se recomienda no editar ni eliminar la instancia del servidor.
-// Instancia del servidor
-const server = app.listen(PORT, () => {
-  console.log(`listening on port http://localhost:${PORT}`);
+  if (!token) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    // Verifica el rol y agrega el rol a los cabeceros del request
+    if (decoded.rol === 'admin') {
+      req.headers = { ...req.headers, rol: 'admin' };
+    } else if (decoded.rol === 'user') {
+      req.headers = { ...req.headers, rol: 'user' };
+    }
+
+    next();
+  });
+}
+
+// Ruta para premium-clients
+app.get('/premium-clients', JWTValidation, (req, res) => {
+  if (req.headers.rol === 'admin') {
+    res.send('premium-clients list');
+  } else {
+    res.status(403).json({ error: 'Access not allowed' });
+  }
 });
 
-// Exportación del servidor
-module.exports = server;
+// Ruta para medium-clients
+app.get('/medium-clients', JWTValidation, (req, res) => {
+  if (req.headers.rol === 'admin' || req.headers.rol === 'user') {
+    res.send('medium-clients list');
+  } else {
+    res.status(403).json({ error: 'Access not allowed' });
+  }
+});
+
+// Resto de tus rutas y configuraciones...
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
+
